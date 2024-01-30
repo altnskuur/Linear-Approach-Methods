f552d555c53c3adfd3db8a839f95b6e9ee91369a
Ugur ALTINISIK <ualtinisik@etu.edu.tr>
2024-01-30 23:59:29 +0300

    methods_function.m complated

--- main.m
+++ main.m
@@ -1,24 +1,10 @@
-% A_matrix_35 = readmatrix("A_Data35.xlsx");
-% b_vector_35 = readmatrix("B_Data35.xlsx");
-% 
-% %x_vector_35Cho = methods_function(A_matrix_35, b_vector_35, 1);
-% x_vector_35GS = methods_function(A_matrix_35, b_vector_35, 2);
-% x_vector_35CGM = methods_function(A_matrix_35, b_vector_35, 3);
-% %%
-% A_matrix_64 = readmatrix("A_Data51.xlsx");
-% b_vector_64 = readmatrix("B_Data51.xlsx");
-% 
-% %x_vector_64Cho = methods_function(A_matrix_64, b_vector_64, 1);
-% x_vector_64GS = methods_function(A_matrix_64, b_vector_64, 2);
-% x_vector_64CGM = methods_function(A_matrix_64, b_vector_64, 3);
-% %%
-% A_matrix_200 = readmatrix("A_Data200.xlsx");
-% b_vector_200 = readmatrix("B_Data200.xlsx");
-% 
-% %x_vector_200Cho = methods_function(A_matrix_200, b_vector_200, 1);
-% x_vector_200GS = methods_function(A_matrix_200, b_vector_200, 2);
-% x_vector_200CGM = methods_function(A_matrix_200, b_vector_200, 3);
+A_matrix_35 = randi([0, 15], [35,35]);
+b_vector_35 = randi([0, 15], [35,1]);
+for i = 1:35
+    A_matrix_35(i,i) = 20;
+end
 
-A_matrix = [1 2 3; 1 2 2; 3 5 4];
-b_vector = [1 2 5];
-x_vector = methods_function(A_matrix, b_vector, 2);
+A_matrix = [8 2 3; 2 8 5; 3 5 8];
+b_vector = [1; 2; 5];
+x_vector = methods_function(A_matrix_35, b_vector_35, 3)
+
--- methods_function.m
+++ methods_function.m
@@ -1,9 +1,7 @@
 function x_vector = methods_function(A_matrix, b_vector, method_number)
     tic %time start
     %Ax=b parameters
-    detA = det(A_matrix); %detA != 0 means linear independent system
     lengthB = length(b_vector);
-    %AB_matrix = [A_matrix b_vector]; %ERROR
     x_vector = zeros(lengthB, 1); %initialize x matrix
     if(method_number < 1 || method_number > 3)
         message = 'Your method_number input parameter is not in [1,3] range.';
@@ -11,49 +9,56 @@ function x_vector = methods_function(A_matrix, b_vector, method_number)
     end
     switch method_number
         case 1 %Cholesky Method
-								L = zeros(lengthB, lengthB);
-								sumofL = 0;
-								for j = 1 : lengthB
-    								 for i = j : lengthB
-        					     if(i==j)
-            					      for k = 1 : (j-1);
-                						    sumofL = L(j,k)*L(j,k) + sumofL;
-            						     end
-            						 L(i,j) = sqrt(A_matrix(i,j) - sumofL);
-        								 else
-            									for k = 1 : (j-1)
-                						    sumofL = L(i,k)*L(j,k);
-                         end
-                         L(i,j) = (A_matrix(i,j) - sumofL) / L(j,j);
-                     end
-                     sumofL = 0;
-              		end
-             	end
-							 		
-									L_T = L.'; %transpose of  matrix
-									%L*y=b and L_T*x=y
-									y_vector = zeros(lengthB, 1);
-									sumofY = 0;
-									for i = 1 : lengthB
-    									for j = 1 : (i-1)
-        								sumofY = L(i,j) * y_vector(j, 1) + sumofY;
-    									end
-    									y_vector(i) = (b_vector(i) - sumofY) / L(i, i);
-    									sumofY = 0;
-									end
+            symmetry_test = issymmetric(A_matrix);
+            if 1 ~= symmetry_test
+                message = 'Given A matrix is not symmetric. Cholesky Method cannot be used.';
+                error(message);
+            end
+            L = zeros(lengthB, lengthB);
+			sumofL = 0;
+            for j = 1 : lengthB
+                for i = j : lengthB
+                    if(i==j)
+            		    for k = 1 : (j-1)
+                		    sumofL = L(j,k)*L(j,k) + sumofL;
+            		    end
+            		    L(i,j) = sqrt(A_matrix(i,j) - sumofL);
+        			else
+                        for k = 1 : (j-1)
+                		    sumofL = L(i,k)*L(j,k) + sumofL;
+                        end
+                        L(i,j) = (A_matrix(i,j) - sumofL) / L(j,j);
+                    end
+                    sumofL = 0;
+                end
+            end				 		
+            %L*L_T*x=b -> L_T*x=y and L*y=b
+            %L*y=b 
+            y_vector = zeros(lengthB, 1);
+            sumofY = 0;
+            for i = 1 : lengthB
+                for j = 1 : (i-1)
+                    sumofY = L(i,j) * y_vector(j) + sumofY;
+                end
+                y_vector(i) = (b_vector(i) - sumofY) / L(i, i);
+                sumofY = 0;
+            end
 
-									sumofX = 0;
-									for j = 1: lengthB
-    									j_T = lengthB - j + 1;
-    									for i = (j_T+1) : lengthB
-        								sumofX = L(i, j_T)*x_vector(i);
-    									end
-    									x_vector(j_T) = (b_vector(j_T) - sumofX) / L(j_T,j_T);
-											sumofX = 0;
-									end
+            %L_T*x=y
+            L_T = L.'; %transpose of  matrix
+            sumofX = 0;
+            for j = 1: lengthB
+                j_T = lengthB - j + 1;
+                for i = (j_T+1) : lengthB
+                    sumofX = L_T(i, j_T)*x_vector(i) + sumofX;
+                end
+    			x_vector(j_T) = (y_vector(j_T) - sumofX) / L_T(j_T,j_T);
+				sumofX = 0;
+            end
         case 2 %Gauss Seidel Method
+            %check matrix is diagonally dominant matrix or not?
             sumofX = zeros(lengthB, 1);
-            for iteration = 1 : 10 %CHANGE: iteration can be dynamic. if change percentage of result is under of a number, iteration can be stop.
+            for iteration = 1 : 300 %CHANGE: iteration can be dynamic. if change percentage of result is under of a number, iteration can be stop.
                 for t = 1 : lengthB
                     for k = 1 : lengthB
                         sumofX(t) = sumofX(t) + A_matrix(t, k) * x_vector(k);
@@ -64,19 +69,16 @@ function x_vector = methods_function(A_matrix, b_vector, method_number)
             end
 
         case 3 %CGM
-            s = b_vector - A_matrix * x_vector;
-            r = b_vector - A_matrix * x_vector;
-            a = (s.' * r) / (s.' * A_matrix * s); %Warning: Matrix is singular, close to singular or badly scaled. Results may be inaccurate. RCOND = NaN.
-            b = 0;
-            x_vector = x_vector + a * s;
-            for j = 1 : 13
-                r = b_vector - A_matrix * x_vector;
-                b = (r.' * A_matrix * s) / (s.' * A_matrix * s); %Warning: Matrix is singular, close to singular or badly scaled. Results may be inaccurate. RCOND = NaN.
-                s = r + b * s;
-                a = (s.' * r) / (s.' * A_matrix * s); %Warning: Matrix is singular, close to singular or badly scaled. Results may be inaccurate. RCOND = NaN.
-                x_vector = x_vector + a * s; %out x_vector = [NaN NaN NaN; NaN NaN NaN; NaN NaN NaN]
+            s = b_vector(:,1) - A_matrix * x_vector(:,1);
+            a = (s.' * (b_vector - A_matrix * x_vector(:,1))) * pinv(s.' * A_matrix * s);
+            x_vector(:,1) = x_vector(:,1) + a * s;
+            for j = 1 : 150
+                r = b_vector(:,1) - A_matrix * x_vector(:,1);
+                B = (-r.' * A_matrix * r) * pinv(s.' * A_matrix * s);
+                s = r + B * s;
+                a = (s.' * (b_vector - A_matrix * x_vector(:,1))) * pinv(s.' * A_matrix * s); 
+                x_vector(:,1) = x_vector(:,1) + a * s;
             end
     end
     toc %time stop
-
 end
